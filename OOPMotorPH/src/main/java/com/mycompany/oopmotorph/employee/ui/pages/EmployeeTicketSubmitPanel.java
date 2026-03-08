ffdf4dd366756d4fe93faeba75a544d986658384
lccpral <lr.lgcorporal@mmdc.mcl.edu.ph>
2026-03-08 22:45:05 +0300

    Add license comment to EmployeeTicketSubmitPanel.java

--- OOPMotorPH/src/main/java/com/mycompany/oopmotorph/employee/ui/pages/EmployeeTicketSubmitPanel.java
+++ OOPMotorPH/src/main/java/com/mycompany/oopmotorph/employee/ui/pages/EmployeeTicketSubmitPanel.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.oopmotorph.employee.ui.pages;
 
 import com.mycompany.oopmotorph.ticket.model.Ticket;
